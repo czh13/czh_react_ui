56123be243b5e66a48b1695693ac1ffb8afb6b71
milton <caizhihao@guidefuture.com>
2023-12-29 14:49:04 +0800

    fix: 1

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,7 +1,18 @@
+/*
+ * @Author: milton
+ * @Date: 2023-12-29 11:49:22
+ * @LastEditors: milton caizhihao@guidefuture.com
+ * @LastEditTime: 2023-12-29 14:45:32
+ * @FilePath: \dumi_react_ui\.dumirc.ts
+ * @Description:
+ *
+ */
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
   outputPath: 'docs-dist',
+  base: '/czh_react_ui/',
+  publicPath: '/czh_react_ui/',
   themeConfig: {
     name: 'czh',
   },
