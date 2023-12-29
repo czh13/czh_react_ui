d847f24d4f6c22d912effd593f45055391859e62
milton <caizhihao@guidefuture.com>
2023-12-29 12:07:25 +0800

    fix: init

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   outputPath: 'docs-dist',
-  base: '/czh_react_ui/',
-  publicPath: '/czh_react_ui/',
   themeConfig: {
     name: 'czh',
   },
