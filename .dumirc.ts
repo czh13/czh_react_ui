f6f14014684bf6924c587d0cb2105f0700232574
milton <caizhihao@guidefuture.com>
2023-12-29 09:59:23 +0800

    init

---
+++ .dumirc.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  outputPath: 'docs-dist',
+  base: '/czh_react_ui/',
+  publicPath: '/czh_react_ui/',
+  themeConfig: {
+    name: 'czh',
+  },
+});
