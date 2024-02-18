865fdd3d50829b70a7d3634272263a467b84de62
milton <caizhihao@guidefuture.com>
2024-02-18 17:17:52 +0800

    feat: ellipsis组件

--- src/Ellipsis/demo/base.tsx
+++ src/Ellipsis/demo/base.tsx
@@ -7,13 +7,13 @@ const Base = () => {
   return (
     <DemoWrap>
       <DemoBlock title="一行省略" style={{ padding: 20 }}>
-        <Ellipsis text="React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes" />
+        <Ellipsis text="Migrates a Create React App project to Next.js; creating a Pages Router and necessary config to match behavior. Client-side only rendering is leveraged initially to prevent breaking compatibility due to window usage during SSR and can be enabled seamlessly to allow the gradual adoption of Next.js specific features." />
       </DemoBlock>
 
       <DemoBlock title="多行省略" style={{ padding: 20 }}>
         <Ellipsis
           rows={3}
-          text="React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes"
+          text="Migrates a Create React App project to Next.js; creating a Pages Router and necessary config to match behavior. Client-side only rendering is leveraged initially to prevent breaking compatibility due to window usage during SSR and can be enabled seamlessly to allow the gradual adoption of Next.js specific features."
         />
       </DemoBlock>
 
@@ -21,14 +21,14 @@ const Base = () => {
         <Ellipsis
           expand="展开"
           collapse="收起"
-          text="React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes"
+          text="Migrates a Create React App project to Next.js; creating a Pages Router and necessary config to match behavior. Client-side only rendering is leveraged initially to prevent breaking compatibility due to window usage during SSR and can be enabled seamlessly to allow the gradual adoption of Next.js specific features."
         />
       </DemoBlock>
 
       <DemoBlock title="仅展开" style={{ padding: 20 }}>
         <Ellipsis
           expand="展开"
-          text="React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes"
+          text="Migrates a Create React App project to Next.js; creating a Pages Router and necessary config to match behavior. Client-side only rendering is leveraged initially to prevent breaking compatibility due to window usage during SSR and can be enabled seamlessly to allow the gradual adoption of Next.js specific features."
         />
       </DemoBlock>
     </DemoWrap>
